02a3d9bc5fa54a96b81d75ac140f5bda4dae0db4
Amit Gulati <agulati@tunecore.com>
2014-06-26 14:14:49 -0400

    Added cvv as optional param for smart_ps.purchase
    Some issuers require the cvv for authorizing a transaction but the cvv
    is not stored in Braintree’s system. Therefore we need to be able to
    pass the cvv during a purchase that us using the Braintree customer
    vault.

--- lib/active_merchant/billing/gateways/smart_ps.rb
+++ lib/active_merchant/billing/gateways/smart_ps.rb
@@ -43,6 +43,7 @@ module ActiveMerchant #:nodoc:
         add_currency(post, money, options)
         add_taxes(post, options)
         add_processor(post, options)
+        add_cvv(post, options)
         commit('sale', money, post)
       end
 
@@ -205,6 +206,10 @@ module ActiveMerchant #:nodoc:
 
       def add_transaction(post, auth)
         post[:transactionid] = auth
+      end
+
+      def add_cvv(post, options)
+        post[:cvv] = options[:cvv] if options[:cvv]
       end
 
       def parse(body)
